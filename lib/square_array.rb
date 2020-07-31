483730f76da0d4bb29568bb2ffdcfe4fdb7193b6
Percy Major <chipercy@hotmail.com>
2020-07-31 17:03:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,9 @@
 def square_array(numbers)
   count = 0
   numbers = [1,2,3]
-    while count < numbers.length do
+    while count < numbers.length 
         puts numbers.map { |e| e ** 2 }
         count += 1
     end
 end
 
- 
